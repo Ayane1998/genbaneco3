9ee7c839784b39b1b3ed45ae3bb691f7234e6c59
Ayane1998 <linzilanze75@gmail.com>
2025-09-19 17:31:46 +0900

    フォント

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,23 @@
 // app/layout.tsx
-import './globals.css';
-import Header from './components/Header';
+import './globals.css'
+import Header from './components/Header'
+import { Zen_Kurenaido } from 'next/font/google'
+
+// フォントを読み込み
+const zenKurenaido = Zen_Kurenaido({
+  weight: '400',        // Zen Kurenaido は 400 のみ
+  subsets: ['latin'],   // 日本語もここでOK
+  display: 'swap',
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={zenKurenaido.className}>
       <body>
         <Header />
         {children}
       </body>
     </html>
-  );
+  )
 }
+
